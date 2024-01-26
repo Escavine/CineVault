518126a8f35457951ba050731cb2982964eaa24b
meowuscat <118561006+Escavine@users.noreply.github.com>
2024-01-26 11:46:27 +0000

    Add files via upload

---
+++ StaffLogin.cpp
@@ -0,0 +1,16 @@
+#include "StaffLogin.h"
+#include "iostream"
+
+StudentMonitor::DatabaseOperations::DatabaseOperations()
+{
+}
+
+StudentMonitor::DatabaseOperations::~DatabaseOperations()
+{
+
+}
+
+bool StudentMonitor::DatabaseOperations::AuthenticateUser(System::String^ username, System::String^ password)
+{
+	return false;
+}
